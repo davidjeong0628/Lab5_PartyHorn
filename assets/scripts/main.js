1fc9f5d1793818fec437908d0d33f179cbc8d4a3
yoonho0628 <davidjeong0628@gmail.com>
2021-02-06 12:21:48 -0800

    Make button play sound

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,36 @@
-// main.js
+const audio = document.getElementById("horn-sound");
+const btn = document.getElementById("honk-btn");
+const img = document.getElementById("sound-image");
 
-// TODO
+const audioAirHorn = "assets/media/audio/air-horn.mp3";
+const radioAirHorn = document.getElementById("radio-air-horn");
+const imgAirHorn = "assets/media/images/air-horn.svg";
+
+const radioCarHorn = document.getElementById("radio-car-horn");
+
+const radioPartyHorn = document.getElementById("radio-party-horn");
+
+
+btn.addEventListener("click", (event) => {
+    playAudio(event);
+});
+
+function playAudio(event) {
+    console.log("play audio");
+    event.preventDefault();
+    audio.play(); 
+}
+
+radioAirHorn.addEventListener("change", setHorn);
+radioCarHorn.addEventListener("change", setHorn);
+radioPartyHorn.addEventListener("change", setHorn);
+
+function setHorn() {
+    if (radioAirHorn.checked) {
+
+    } else if (radioCarHorn.checked) {
+
+    } else if (radioPartyHorn.checked) {
+
+    }
+}
